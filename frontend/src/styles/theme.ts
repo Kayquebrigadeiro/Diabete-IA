5cf372b34c686059dcaa60221480fc6b5b77ded9
ExodiaD <pazyuri84@gmail.com>
2026-06-09 22:07:36 -0300

    fix: adiciona 25º elemento no array shadows

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -181,6 +181,7 @@ export const createAppTheme = (mode: 'light' | 'dark') => {
       isDark ? '0 25px 50px -12px rgba(0, 0, 0, 0.5)' : '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
       isDark ? '0 25px 50px -12px rgba(0, 0, 0, 0.5)' : '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
       isDark ? '0 25px 50px -12px rgba(0, 0, 0, 0.5)' : '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+      isDark ? '0 25px 50px -12px rgba(0, 0, 0, 0.5)' : '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
     ] as const,
     components: {
       MuiCssBaseline: {
